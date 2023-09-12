c50df1da7470ce0d311b2283dd3932555931355c
dspwebstudio <dspwebstudio@gmail.com>
2023-09-12 17:59:03 -0600

    ghpages

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,7 +7,7 @@ const Card = () => {
             <h2 className="card__title">Título</h2>
             <p className="card__text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque ipsam asperiores ipsum deserunt. Sit, voluptatem!</p>
             <a href="http://google.com" rel="noopener noreferrer" target="_blank" >
-                <button className="button button--green">
+                <button className="button button--black">
                     <BiSearchAlt className='button__svg' />
                     <span className='button__title'>Mas Información</span>
                 </button>
